d122386ab76ea3ce5baec0d2325aa64dc88fa714
CrosscutX <crosscutcode@gmail.com>
2023-08-25 14:48:02 -0400

    Property image added

--- src/components/Property.jsx
+++ src/components/Property.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import House from "../assets/houses.jpg";
 
 export default function Property() {
   return (
     <div className="property">
       <div className="property-box">
-        <div className="property-photo-container"></div>
+        <div className="property-photo-container">
+          <img src={House} alt="House Photo" />
+        </div>
         <div className="property-info-container"></div>
       </div>
     </div>
