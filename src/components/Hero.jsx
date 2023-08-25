d53cad7151c8079380b78abb64629f26c7896e65
CrosscutX <crosscutcode@gmail.com>
2023-08-25 13:46:39 -0400

    Basic Setup

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <h1>Ace Management Listing Portal</h1>
+    </div>
+  );
+}
---
+++ src/components/Property.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Property() {
+  return <div className="property">Property</div>;
+}
